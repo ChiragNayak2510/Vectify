cb2fe77c4fe30f71bf95adf2e2e3078041c1a2c3
ChiragNayak2510 <chiragnayak251002@gmail.com>
2024-09-07 21:12:42 +0530

    Initialised spring boot

---
+++ Vectify/src/main/java/com/example/Vectify/VectifyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vectify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VectifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VectifyApplication.class, args);
+	}
+
+}
---
+++ Vectify/src/test/java/com/example/Vectify/VectifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vectify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VectifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
