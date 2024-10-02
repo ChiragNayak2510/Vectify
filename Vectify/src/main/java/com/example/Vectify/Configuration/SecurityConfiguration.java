0725e29fd96d489103e5c3ea37eb9e173175e714
ChiragNayak2510 <chiragnayak251002@gmail.com>
2024-10-02 19:56:24 +0530

    OAuth2 using Google and GitHub added

--- Vectify/src/main/java/com/example/Vectify/Configuration/SecurityConfiguration.java
+++ Vectify/src/main/java/com/example/Vectify/Configuration/SecurityConfiguration.java
@@ -1,4 +1,32 @@
 package com.example.Vectify.Configuration;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/login").permitAll()
+                        .requestMatchers("/api/**").authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                        .permitAll()
+                );
+
+        return http.build();
+    }
 }
